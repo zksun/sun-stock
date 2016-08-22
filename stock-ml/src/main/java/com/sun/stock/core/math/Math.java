42d5c18d77a82c8c87a3b3370e9e279b941c0afd
缉熙 <zhikun.szk@alibaba-inc.com>
2016-08-22 21:51:51 +0800

    ml module下的类库

--- stock-ml/src/main/java/com/sun/stock/core/math/Math.java
+++
@@ -1,7 +0,0 @@
-package com.sun.stock.core.math;
-
-/**
- * Created by hanshou on 8/22/16.
- */
-public class Math {
-}
---
+++ stock-ml/src/main/java/com/sun/stock/ml/com/sun/stock/ml/math/Math.java
@@ -0,0 +1,7 @@
+package com.sun.stock.ml.com.sun.stock.ml.math;
+
+/**
+ * Created by zksun on 8/22/16.
+ */
+public class Math {
+}
