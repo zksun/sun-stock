070c169b1c60da368ca0507d424778de663f42fb
缉熙 <zhikun.szk@alibaba-inc.com>
2017-08-22 21:35:12 +0800

    ml module下的类库

--- stock-core/src/main/java/com/sun/stock/core/file/ServerDecoder.java
+++ stock-core/src/main/java/com/sun/stock/core/file/ServerDecoder.java
@@ -3,6 +3,7 @@ package com.sun.stock.core.file;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.util.ReferenceCountUtil;
 
 import java.nio.ByteOrder;
 
@@ -20,27 +21,32 @@ public class ServerDecoder extends LengthFieldBasedFrameDecoder {
     protected Object decode(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
         in = (ByteBuf) super.decode(ctx, in);
         if (null == in) return null;
-        if (in.readableBytes() < HEAD_SIZE) {
-            throw new RuntimeException("data low than the head size");
+        try {
+            if (in.readableBytes() < HEAD_SIZE) {
+                throw new RuntimeException("data low than the head size");
+            }
+
+            byte type = in.readByte();
+            int code = in.readIntLE();
+            long time = in.readLongLE();
+            int length = in.readIntLE();
+
+            if (in.readableBytes() < length) {
+                throw new RuntimeException("data low than the protocol length ");
+            }
+
+            FileDO fileDO = new FileDO();
+            fileDO.setType(type);
+            fileDO.setCode(code);
+            fileDO.setTime(time);
+            fileDO.setLength(length);
+            byte[] buf = new byte[length];
+            in.readBytes(buf);
+            fileDO.setDocument(buf);
+
+            return fileDO;
+        } finally {
+            ReferenceCountUtil.release(in);
         }
-
-        byte type = in.readByte();
-        int code = in.readIntLE();
-        long time = in.readLongLE();
-        int length = in.readIntLE();
-
-        if (in.readableBytes() < length) {
-            throw new RuntimeException("data low than the protocol length ");
-        }
-
-        FileDO fileDO = new FileDO();
-        fileDO.setType(type);
-        fileDO.setCode(code);
-        fileDO.setTime(time);
-        fileDO.setLength(length);
-        byte[] buf = new byte[length];
-        in.readBytes(buf);
-        fileDO.setDocument(buf);
-        return fileDO;
     }
 }
