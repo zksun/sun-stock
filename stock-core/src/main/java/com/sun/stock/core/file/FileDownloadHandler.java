a25c400d12062f4b897b196071fa2637dfaf401b
coderzksun@gmail.com <coderzksun@gmail.com>
2017-11-04 15:00:45 +0800

    add maven dependency

--- stock-core/src/main/java/com/sun/stock/core/file/FileDownloadHandler.java
+++ stock-core/src/main/java/com/sun/stock/core/file/FileDownloadHandler.java
@@ -2,8 +2,11 @@ package com.sun.stock.core.file;
 
 import com.sun.stock.core.common.logging.Logger;
 import com.sun.stock.core.common.logging.LoggerFactory;
+import com.sun.stock.core.file.util.FileUtils;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.io.*;
 
 /**
  * Created by zhikunsun on 2017/11/4.
@@ -19,7 +22,41 @@ public class FileDownloadHandler extends SimpleChannelInboundHandler<FileDO> {
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FileDO fileDO) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, FileDO msg) throws Exception {
+        if (null == msg) {
+            ctx.channel().writeAndFlush("failure");
+            throw new RuntimeException("decode no fileDO");
+        }
+
+        if (msg.getType() != 1) {
+            ctx.fireChannelRead(msg);
+            return;
+        }
+
+        String directoryName = FileUtils.getDirectoryName(msg.getType(), msg.getCode());
+        String directory = FileUtils.createDirectory(path, directoryName);
+        File file = FileUtils.filePath(directory, msg.getTime());
+        BufferedInputStream bufferedInputStream = null;
+        try {
+            bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+            int available = bufferedInputStream.available();
+            byte[] fileBytes;
+            bufferedInputStream.read(fileBytes = new byte[available]);
+            if (null != fileBytes || fileBytes.length > 0) {
+                FileDO fileDO = new FileDO();
+                fileDO.setCode(msg.getCode());
+                fileDO.setLength(fileBytes.length);
+                fileDO.setDocument(fileBytes);
+                ctx.writeAndFlush(fileDO);
+            }
+        } catch (Exception e) {
+            logger.error("download file error: ", e);
+        } finally {
+            if (null != bufferedInputStream) {
+                bufferedInputStream.close();
+            }
+        }
+
 
     }
 }
