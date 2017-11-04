4af548699f75b5895a19f26fe07dc92aa063a7c3
coderzksun@gmail.com <coderzksun@gmail.com>
2017-11-05 00:29:20 +0800

    add maven dependency

---
+++ stock-core/src/test/java/com/sun/stock/core/test/CommonTest.java
@@ -0,0 +1,17 @@
+package com.sun.stock.core.test;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+/**
+ * Created by zhikunsun on 2017/11/5.
+ */
+public class CommonTest {
+    @Test
+    public void todayTest() {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate.getDayOfWeek().toString());
+        System.out.println(localDate.getMonth().toString());
+    }
+}
